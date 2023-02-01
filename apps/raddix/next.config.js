5bbc7de4d06aff03611f6c5121f54a77c858e2db
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-01 16:58:08 -0500

    feat(raddix): change @stitches to sass

--- apps/raddix/next.config.js
+++ apps/raddix/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en'
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
   }
 };
 
