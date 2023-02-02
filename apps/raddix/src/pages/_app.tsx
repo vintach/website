77251889476867e98fba63ccd289a57dd894e4db
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-01 21:17:54 -0500

    feat(raddix): update app file

--- apps/raddix/src/pages/_app.tsx
+++ apps/raddix/src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AppLayout } from '@/layouts/app';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
+  );
 }
