f3162dcea985e12c5a8995fe64061006c6182883
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-16 12:57:36 -0500

    feat(raddix): add code block component

---
+++ apps/raddix/src/components/code-block/code-block.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { JetBrains_Mono } from '@next/font/google';
+import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+import classNames from 'classnames';
+import { blameTheme } from './theme';
+import styles from './code-block.module.scss';
+import { getFileExtension } from '@/utils/global';
+
+const inter = JetBrains_Mono({ subsets: ['latin'] });
+
+interface CodeBlockProps {
+  tabs: {
+    [key: string]: string;
+  };
+}
+
+export const CodeBlock = ({ tabs }: CodeBlockProps) => {
+  const [activeTab, setActiveTab] = useState(0);
+  const lines = tabs[Object.keys(tabs)[activeTab]].trim();
+  const tabName = Object.keys(tabs)[activeTab];
+  const language = getFileExtension(tabName);
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.windowTabs}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <>
+        <Highlight
+          {...defaultProps}
+          code={lines}
+          language={language as Language}
+          theme={blameTheme}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre
+              className={classNames(
+                className,
+                inter.className,
+                styles.editorWindows
+              )}
+              style={{
+                ...style
+              }}
+              translate='no'
+            >
+              <div className={styles.code}>
+                {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })} translate='no'>
+                    {line.map((token, key) => {
+                      if (
+                        token.types[token.types.length - 1] === 'string' &&
+                        /^(['"`])\.\/.*?\.(js|jsx|css)\1$/.test(token.content)
+                      ) {
+                        const tab = token.content.substring(
+                          3,
+                          token.content.length - 1
+                        );
+                        const myToken = { ...getTokenProps({ token, key }) };
+
+                        return (
+                          <span
+                            className={myToken.className}
+                            style={myToken.style}
+                            key={myToken.key}
+                          >
+                            <button
+                              onClick={() =>
+                                setActiveTab(Object.keys(tabs).indexOf(tab))
+                              }
+                              className={styles.tokenButton}
+                            >
+                              {myToken.children}
+                            </button>
+                          </span>
+                        );
+                      }
+
+                      return (
+                        <span
+                          {...getTokenProps({ token, key })}
+                          translate='no'
+                        />
+                      );
+                    })}
+                  </div>
+                ))}
+              </div>
+            </pre>
+          )}
+        </Highlight>
+      </>
+    </div>
+  );
+};
---
+++ apps/raddix/src/components/code-block/theme.ts
@@ -0,0 +1,102 @@
+import { PrismTheme } from 'prism-react-renderer';
+
+export const blameTheme: PrismTheme = {
+  plain: {
+    color: '#e5c07b',
+    backgroundColor: 'transparent'
+  },
+  styles: [
+    {
+      types: ['prolog', 'constant', 'builtin'],
+      style: {
+        color: 'rgb(189, 147, 249)'
+      }
+    },
+    {
+      types: ['inserted', 'function', 'maybe-class-name'],
+      style: {
+        color: 'rgb(97, 175, 239)'
+      }
+    },
+    {
+      types: ['deleted'],
+      style: {
+        color: 'rgb(255, 85, 85)'
+      }
+    },
+    {
+      types: ['changed'],
+      style: {
+        color: 'rgb(255, 184, 108)'
+      }
+    },
+    {
+      types: ['punctuation'],
+      style: {
+        color: '#999'
+      }
+    },
+    {
+      types: ['arrow'],
+      style: {
+        color: 'rgb(189, 147, 249)'
+      }
+    },
+    {
+      types: ['string', 'char', 'tag', 'selector', 'attr-value'],
+      style: {
+        color: 'rgb(152, 195, 121)'
+      }
+    },
+    {
+      types: ['keyword', 'variable'],
+      style: {
+        color: 'rgb(198, 120, 221)'
+      }
+    },
+    {
+      types: ['comment'],
+      style: {
+        color: 'rgb(98, 114, 164)'
+      }
+    },
+    {
+      types: ['attr-name'],
+      style: {
+        color: '#d19a66'
+      }
+    },
+    {
+      types: ['tag', 'parameter', 'imports'],
+      style: {
+        color: 'rgb(224, 108, 117)'
+      }
+    },
+    {
+      types: [
+        'class',
+        'selector',
+        'attribute',
+        'attr-name',
+        'number',
+        'hexcode',
+        'color'
+      ],
+      style: {
+        color: '#d19a66'
+      }
+    },
+    {
+      types: ['unit'],
+      style: {
+        color: 'rgb(224, 108, 117)'
+      }
+    },
+    {
+      types: ['property'],
+      style: {
+        color: '#56b6c2'
+      }
+    }
+  ]
+};
