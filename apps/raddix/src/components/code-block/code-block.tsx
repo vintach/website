e83ff324bfc244533270b2ce16e016b198f3bf49
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-17 13:55:35 -0500

    feat(raddix): add custom scrollbar

--- apps/raddix/src/components/code-block/code-block.tsx
+++ apps/raddix/src/components/code-block/code-block.tsx
@@ -1,12 +1,11 @@
-import { Fragment, useState } from 'react';
-import { JetBrains_Mono } from '@next/font/google';
-import Highlight, { defaultProps, Language } from 'prism-react-renderer';
-import classNames from 'classnames';
-import { blameTheme } from './theme';
-import styles from './code-block.module.scss';
 import { getFileExtension } from '@/utils/global';
-
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import styles from './code-block.module.scss';
+import classNames from 'classnames';
+import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+import { JetBrains_Mono } from '@next/font/google';
 const inter = JetBrains_Mono({ subsets: ['latin'] });
+import { blameTheme } from './theme';
 
 interface CodeBlockProps {
   tabs: {
@@ -16,11 +15,104 @@ interface CodeBlockProps {
   showLines?: boolean;
 }
 
-export const CodeBlock = ({ tabs, showTabs, showLines }: CodeBlockProps) => {
-  const [activeTab, setActiveTab] = useState(0);
+export const CodeBlock = ({ tabs, showLines, showTabs }: CodeBlockProps) => {
+  const [activeTab, setActiveTab] = useState(1);
+  const [scrollY, setScrollY] = useState<number>(0);
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrollYHeight, setScrollYHeight] = useState(0);
   const lines = tabs[Object.keys(tabs)[activeTab]].trim();
   const tabName = Object.keys(tabs)[activeTab];
   const language = getFileExtension(tabName);
+  const [scrollThumbPosition, setScrollThumbPosition] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const codeContent = useRef<HTMLDivElement>(null);
+  const codeWindow = useRef(null);
+
+  const handleScroll = () => {
+    const codeConten = codeContent.current;
+    if (!codeConten) return;
+
+    const { scrollTop, scrollHeight, offsetHeight } = codeConten;
+    // console.log({ scrollHeight });
+    // console.log({ offsetHeight });
+    console.log(scrollYHeight);
+    setScrollTop(scrollTop);
+
+    let newTop =
+      (parseInt(`${scrollTop}`, 10) / parseInt(`${scrollHeight}`, 10)) *
+      offsetHeight;
+
+    newTop = Math.min(newTop, offsetHeight - scrollYHeight);
+    console.log({ newTop: Number(newTop.toFixed()) });
+
+    setScrollY(Number(newTop.toFixed()));
+    // console.log({ newTop });
+  };
+
+  const handleDocumentMouseUp = (e: MouseEvent) => {
+    if (isDragging) {
+      e.preventDefault();
+      setIsDragging(false);
+    }
+  };
+
+  const handleDocumentMouseMove = (e: MouseEvent) => {
+    if (isDragging) {
+      e.preventDefault();
+      e.stopPropagation();
+      const scrollHostElement = codeContent.current;
+      if (!scrollHostElement) return;
+      const { scrollHeight, offsetHeight } = scrollHostElement;
+
+      let deltaY = e.clientY - scrollThumbPosition;
+      let percentage = deltaY * (scrollHeight / offsetHeight);
+
+      setScrollThumbPosition(e.clientY);
+      setScrollY(
+        Math.min(Math.max(0, scrollY + deltaY), offsetHeight - scrollYHeight)
+      );
+      scrollHostElement.scrollTop = Math.min(
+        scrollHostElement.scrollTop + percentage,
+        scrollHeight - offsetHeight
+      );
+    }
+  };
+
+  const handleMouseDownY = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setScrollThumbPosition(e.clientY);
+    setIsDragging(true);
+  };
+
+  useEffect(() => {
+    const codeConten = codeContent.current;
+    if (!codeConten) return;
+    const scrollYPorcentage = codeConten.clientHeight / codeConten.scrollHeight;
+    const scrollThumbY = Math.max(
+      scrollYPorcentage * codeConten.clientHeight,
+      20
+    );
+    setScrollYHeight(Number(scrollThumbY.toFixed()));
+
+    codeConten.addEventListener('scroll', handleScroll);
+    return () => {
+      codeConten.removeEventListener('scroll', handleScroll);
+    };
+  }, [activeTab, scrollYHeight]);
+
+  useEffect(() => {
+    //this is handle the dragging on scroll-thumb
+    document.addEventListener('mousemove', handleDocumentMouseMove);
+    document.addEventListener('mouseup', handleDocumentMouseUp);
+    document.addEventListener('mouseleave', handleDocumentMouseUp);
+    return function cleanup() {
+      document.removeEventListener('mousemove', handleDocumentMouseMove);
+      document.removeEventListener('mouseup', handleDocumentMouseUp);
+      document.removeEventListener('mouseleave', handleDocumentMouseUp);
+    };
+  }, [handleDocumentMouseMove, handleDocumentMouseUp, activeTab]);
 
   return (
     <div className={styles.root}>
@@ -44,40 +136,54 @@ export const CodeBlock = ({ tabs, showTabs, showLines }: CodeBlockProps) => {
           ))}
         </div>
       )}
-      <>
-        <Highlight
-          {...defaultProps}
-          code={lines}
-          language={language as Language}
-          theme={blameTheme}
-        >
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre
-              className={classNames(
-                className,
-                inter.className,
-                styles.editorWindows
-              )}
-              style={{
-                ...style
-              }}
-              translate='no'
-            >
-              {showLines && (
-                <div className={styles.lines} aria-hidden='true'>
-                  {Array.from({ length: tokens.length + 1 }).map((_, i) => {
-                    return i === 0 ? (
-                      i + 1
-                    ) : (
-                      <Fragment key={i + 1}>
-                        <br />
-                        {i + 1}
-                      </Fragment>
-                    );
-                  })}
-                </div>
-              )}
-              <div className={styles.code}>
+      <Highlight
+        {...defaultProps}
+        code={lines}
+        language={language as Language}
+        theme={blameTheme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={classNames(
+              className,
+              inter.className,
+              styles.editorWindows
+            )}
+            style={{
+              ...style
+            }}
+            translate='no'
+          >
+            {showLines && (
+              <div
+                className={styles.lines}
+                aria-hidden='true'
+                style={{ top: -scrollTop }}
+              >
+                {Array.from({ length: tokens.length + 1 }).map((_, i) => {
+                  return i === 0 ? (
+                    i + 1
+                  ) : (
+                    <React.Fragment key={i + 1}>
+                      <br />
+                      {i + 1}
+                    </React.Fragment>
+                  );
+                })}
+              </div>
+            )}
+            <div className={styles.codeWrapper} ref={codeWindow}>
+              <div className={styles.scrollbarX}>
+                <span className='slider'></span>
+              </div>
+              <div className={styles.scrollbarY}>
+                <span
+                  onMouseDown={handleMouseDownY}
+                  style={{ height: scrollYHeight, top: scrollY }}
+                ></span>
+              </div>
+
+              <div className={styles.code} ref={codeContent}>
                 {tokens.map((line, i) => (
                   <div {...getLineProps({ line, key: i })} translate='no'>
                     {line.map((token, key) => {
@@ -119,10 +225,10 @@ export const CodeBlock = ({ tabs, showTabs, showLines }: CodeBlockProps) => {
                   </div>
                 ))}
               </div>
-            </pre>
-          )}
-        </Highlight>
-      </>
+            </div>
+          </pre>
+        )}
+      </Highlight>
     </div>
   );
 };
