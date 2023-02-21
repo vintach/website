98637c799f2faca236fdb17737bac55a70a64780
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-20 23:36:47 -0500

    feat(raddix): add useScroll hook

---
+++ apps/raddix/src/hooks/useScroll.ts
@@ -0,0 +1,45 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+interface Element {
+  scrollTop: number;
+  scrollLeft: number;
+  scrollHeight: number;
+  scrollWidth: number;
+}
+
+const defaultState: Element = {
+  scrollTop: 0,
+  scrollLeft: 0,
+  scrollHeight: 0,
+  scrollWidth: 0
+};
+
+const useScroll = <T extends HTMLElement = any>() => {
+  const [element, setElement] = useState<Element>(defaultState);
+  const ref = useRef<T>(null);
+
+  const handleScroll = useCallback(() => {
+    if (ref.current) {
+      const refElement = ref.current;
+      setElement({
+        scrollTop: refElement.scrollTop,
+        scrollLeft: refElement.scrollLeft,
+        scrollHeight: refElement.scrollHeight,
+        scrollWidth: refElement.scrollWidth
+      });
+    }
+  }, [element]);
+
+  useEffect(() => {
+    const refElement = ref.current;
+    if (!refElement) return;
+    refElement.addEventListener('scroll', handleScroll);
+    return () => {
+      refElement.removeEventListener('scroll', handleScroll);
+    };
+  }, [ref.current]);
+
+  return [ref, element] as const;
+};
+
+export default useScroll;
