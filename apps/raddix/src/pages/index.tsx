7222c8a0c539059c02f3ff4b83be0fb32268e029
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-22 21:54:46 -0500

    feat(raddix): remove feature component

--- apps/raddix/src/pages/index.tsx
+++ apps/raddix/src/pages/index.tsx
@@ -1,14 +1,12 @@
 import { Hero } from '@/components/hero';
-import { Features } from '@/components/features';
 import { HomeLayout } from '@/layouts/home';
 import { Styling } from '@/components/styling';
+import { Accessible } from '@/components/home/accessible';
 
 export default function Home() {
   return (
     <HomeLayout>
       <Hero />
-
-      <Features />
       <Styling />
     </HomeLayout>
   );
