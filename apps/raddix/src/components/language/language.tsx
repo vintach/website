70d02bde0bdc31c7fbe58e218c0fba9f73202b79
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-02-24 09:43:24 -0500

    feat(raddix): add translate component

---
+++ apps/raddix/src/components/language/language.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './language.module.scss';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+
+export const Language = () => {
+  const { locales, asPath } = useRouter();
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const refBtn = useRef<HTMLButtonElement>(null);
+
+  const handleClickOutside = (e: MouseEvent | TouchEvent) => {
+    const button = refBtn.current;
+
+    if (!button || button.contains(e.target as Node)) {
+      return;
+    }
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, [refBtn, setIsActive]);
+
+  return (
+    <div className={styles.language}>
+      <button ref={refBtn} onClick={() => setIsActive(!isActive)}>
+        <Image
+          src={'/icons/languaje.svg'}
+          alt='language'
+          width={25}
+          height={25}
+        />
+      </button>
+      <div className={styles.menu} style={{ opacity: isActive ? 1 : 0 }}>
+        <ul>
+          {locales?.map(localName => {
+            return (
+              <li key={localName}>
+                <Link href={asPath} passHref locale={localName}>
+                  {localName === 'en' ? 'English' : 'Español'}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
