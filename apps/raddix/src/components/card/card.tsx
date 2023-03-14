93b550c3d1ed13af888b5f8c203690a77c396a97
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-03-13 21:20:11 -0500

    feat(raddix): update Props in card component

--- apps/raddix/src/components/card/card.tsx
+++ apps/raddix/src/components/card/card.tsx
@@ -1,3 +1,4 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react';
 import style from './card.module.scss';
 import { Children } from '@/types/global';
 
@@ -5,15 +6,27 @@ export const CardGroup = ({ children }: Children) => {
   return <div className={style.cardGroup}>{children}</div>;
 };
 
-interface CardProps {
-  as?: keyof JSX.IntrinsicElements;
+type CardProps<E extends ElementType> = {
+  as?: E;
   title?: string;
   text?: string;
-}
+};
 
-export const Card = ({ as: Comp = 'div', title, text }: CardProps) => {
+type Props<E extends React.ElementType> = React.PropsWithChildren<
+  CardProps<E>
+> &
+  Omit<ComponentPropsWithoutRef<E>, keyof CardProps<E>>;
+
+export const Card = <E extends ElementType = 'div'>({
+  as,
+  title,
+  text,
+  ...rest
+}: Props<E>) => {
+  const Comp = as || 'div';
+
   return (
-    <Comp className={style.card}>
+    <Comp className={style.card} {...rest}>
       {title && <h4>{title}</h4>}
       {text && <p>{text}</p>}
     </Comp>
