9c8fe10550db275cf19224faf9a238d14a93c1dc
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-03-26 18:54:44 -0500

    feat(raddix): update switch component

--- apps/raddix/src/demo/switch/switch.tsx
+++ apps/raddix/src/demo/switch/switch.tsx
@@ -2,7 +2,9 @@ import { useSwitch, UseSwitchProps } from '@raddix/switch';
 import styles from './switch.module.scss';
 
 export const Switch = (props: UseSwitchProps) => {
-  const { switchProps } = useSwitch(props);
+  const { switchProps, dataProps } = useSwitch(props);
 
-  return <button {...switchProps} className={styles.switch}></button>;
+  return (
+    <button {...switchProps} {...dataProps} className={styles.switch}></button>
+  );
 };
