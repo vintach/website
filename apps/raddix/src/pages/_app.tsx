9be3195d25ad5d936dc19ca9614047536ad20d9c
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-04-05 17:23:59 -0500

    feat(raddix): add vercel analytics

--- apps/raddix/src/pages/_app.tsx
+++ apps/raddix/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AppLayout } from '@/layouts/app';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <AppLayout>
         <Component {...pageProps} />
+        <Analytics />
       </AppLayout>
     </>
   );
