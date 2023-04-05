01f2aad6002bf1c72d5016a3b1dd5d9958682d82
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-04-04 22:45:31 -0500

    feat(raddix): update footer component

--- apps/raddix/src/components/footer/footer.tsx
+++ apps/raddix/src/components/footer/footer.tsx
@@ -1,16 +1,72 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>
-        A project by{' '}
-        <a href='https://www.moisesmachuca.com/' target='_blank'>
-          Moisés Machuca
-        </a>
-      </p>
+      <div className={styles.sections}>
+        <div>
+          <div className={styles.logo}>
+            <div>
+              <Image
+                src='/raddix.svg'
+                alt='Raddix logo'
+                width={24}
+                height={36}
+              />
+              <h1>raddix</h1>
+            </div>
+            <span>A project by Vintach</span>
+          </div>
+        </div>
+
+        <div>
+          <h3>Getting Started</h3>
+          <ul>
+            <li>
+              <Link href={'/docs/get-started/add-to-project'}>
+                Add a your project
+              </Link>
+            </li>
+            <li>
+              <Link href={'/docs/get-started/create-component'}>
+                Creating a component
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Concepts</h3>
+          <ul>
+            <li>
+              <Link href={'/docs/concepts/why-raddix'}>Why Raddix</Link>
+            </li>
+            <li>
+              <Link href={'/docs/concepts/accesibility'}>Accesibility</Link>
+            </li>
+            <li>
+              <Link href={'/docs/concepts/customization'}>Customization</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Comnity</h3>
+          <ul>
+            <li>
+              <Link href={'https://github.com/vintach/raddix'}>Github</Link>
+            </li>
+            <li>
+              <Link href={'https://github.com/vintach/raddix/issues'}>
+                Issuses
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className={styles.deploy}>
         <span>Deployed on</span>
         <Image src={'/icons/vercel.svg'} alt='deploy' width={80} height={18} />
