4efbce6cd0d8925852c3d8ceb8a05af46e085ede
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-05-31 15:13:00 -0500

    feat: add color system of vintach

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { colors } = require('@vintach/colors');
+
 module.exports = {
   content: [
     'src/**/*.{js,ts,jsx,tsx}',
@@ -11,43 +14,17 @@ module.exports = {
       md: '56.25rem', //900px
       lg: '73.75rem' //1180px
     },
+    colors,
     fontSize: {
-      xs: [
-        '.875rem', //14px
-        '1.5rem' // 24px
-      ],
-      sm: [
-        '1rem', //16px
-        '1.625rem' //26px
-      ],
-      md: [
-        '1.125rem', //18px
-        '1.875rem' //30px
-      ],
-      lg: [
-        '1.3125rem', //21px
-        '2rem' //32px
-      ],
-      xl: [
-        '1.5rem', //24px
-        '2.125rem' //34px
-      ],
-      '2xl': [
-        '1.625rem', //26px
-        '2rem' //32px
-      ],
-      '3xl': [
-        '2rem ', //32px
-        '2.4375rem' // 39px
-      ],
-      '4xl': [
-        '2.1875rem', //35px
-        '2.5rem' //40px
-      ],
-      '5xl': [
-        '2.625rem', //42px
-        '3.25rem' //52px
-      ]
+      xs: ['.875rem', '1.5rem'], // 14px-24px
+      sm: ['1rem', '1.625rem'], //16px-26px
+      md: ['1.125rem', '1.875rem'], //18px-30px
+      lg: ['1.3125rem', '2rem'], //21px-32px
+      xl: ['1.5rem', '2.125rem'], //24px-34px
+      '2xl': ['1.625rem', '2rem'], //26px-32px
+      '3xl': ['2rem ', '2.4375rem'], //32px-39px
+      '4xl': ['2.1875rem', '2.5rem'], //35px-40px
+      '5xl': ['2.625rem', '3.25rem'] //42px-52px
     },
     fontFamily: {
       inter: ['Inter', 'sans-serif']
@@ -62,21 +39,6 @@ module.exports = {
         '2xl': '3.3125rem', //53px
         '3xl': '3.9375rem', //63px
         '4xl': '4.5rem' //72px
-      },
-      colors: {
-        brand: {
-          purple: '#9150FF',
-          blue: '#312FF5'
-        },
-        grayscale: {
-          '01': '#EEE7F9',
-          '02': '#EDE6E6',
-          '03': '#B9B7BD',
-          '04': '#A09EA3'
-        }
-      },
-      backgroundColor: {
-        std: '#0A0A10'
       },
       maxWidth: {
         std: '75rem' //1200px
