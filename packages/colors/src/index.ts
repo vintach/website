3e71841326b518114de2881641ca8fee0fc77cef
Moisés Machuca Valverde <rmoimv@gmail.com>
2023-05-30 23:59:22 -0500

    chore: add color system package (#15)

---
+++ packages/colors/src/index.ts
@@ -0,0 +1,11 @@
+import { purple } from './purple';
+import { blue } from './blue';
+import { gray } from './gray';
+
+export const colors = {
+  black: 'rgb(10, 10, 16)',
+  white: 'rgb(240, 240, 240)',
+  purple,
+  blue,
+  gray
+};
