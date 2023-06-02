26c8ed0a2676f36252bee8d745593c380098a312
Moisés Machuca Valverde <rmoimv@gmail.com>
2023-06-01 23:45:48 -0500

    refactor: replace footer component (#21)
    Addd Footer component from packages/vintex and old files removed

--- apps/raddix/src/layouts/app/app.layout.tsx
+++ apps/raddix/src/layouts/app/app.layout.tsx
@@ -1,6 +1,6 @@
-import { Footer } from '@/components/footer';
-import { Header } from '@/components/header';
 import Head from 'next/head';
+import { Footer } from 'vintex';
+import { Header } from '@/components/header';
 
 export interface AppProps {
   children?: React.ReactNode;
