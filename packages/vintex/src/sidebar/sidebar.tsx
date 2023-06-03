48c760c7b7fc31c8be0956e73c234ab6d73cf4fe
Moisés Machuca Valverde <rmoimv@gmail.com>
2023-06-02 23:59:25 -0500

    fix: semantic HTML in sidebar component (#25)
    Changed the h5 to the tag and added the ol tag as the parent of the li list

--- packages/vintex/src/sidebar/sidebar.tsx
+++ packages/vintex/src/sidebar/sidebar.tsx
@@ -13,36 +13,38 @@ export const Sidebar = ({ children }: { children: ReactNode }) => {
 export const SidebarMenu = ({ menu, currentRoute }: SideBarMenuProps) => {
   return (
     <nav className='px-[4.5px]'>
-      {menu.map(({ title, items }, i) => (
-        <li key={`${title}-${i}`} className='pb-md'>
-          <h5 className='pb-sm text-sm font-medium'>{title}</h5>
+      <ol>
+        {menu.map(({ title, items }, i) => (
+          <li key={`${title}-${i}`} className='pb-md'>
+            <span className='block pb-sm text-sm font-medium'>{title}</span>
 
-          <ol>
-            {items.map((item, ii) => {
-              const isActive = item.route.path === currentRoute;
-              const styleLiActive = `relative before:absolute before:left-[-3.5px] before:top-[47%] before:-translate-y-2/4 before:text-md before:text-purple-40 before:content-["•"]`;
-              const styleLinkActive = `font-medium text-purple-40`;
+            <ol>
+              {items.map((item, ii) => {
+                const isActive = item.route.path === currentRoute;
+                const styleLiActive = `relative before:absolute before:left-[-3.5px] before:top-[47%] before:-translate-y-2/4 before:text-md before:text-purple-40 before:content-["•"]`;
+                const styleLinkActive = `font-medium text-purple-40`;
 
-              return (
-                <li
-                  key={`${item.title}${ii}`}
-                  className={isActive ? styleLiActive : undefined}
-                >
-                  <Link
-                    className={`block border-l-2 border-gray-100 py-xs pl-sm text-gray-30  ${
-                      isActive ? styleLinkActive : `hover:text-gray-20`
-                    }`}
-                    href={item.route.path}
-                    locale={item.route.locale}
+                return (
+                  <li
+                    key={`${item.title}${ii}`}
+                    className={isActive ? styleLiActive : undefined}
                   >
-                    {item.title}
-                  </Link>
-                </li>
-              );
-            })}
-          </ol>
-        </li>
-      ))}
+                    <Link
+                      className={`block border-l-2 border-gray-100 py-xs pl-sm text-gray-30  ${
+                        isActive ? styleLinkActive : `hover:text-gray-20`
+                      }`}
+                      href={item.route.path}
+                      locale={item.route.locale}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ol>
+          </li>
+        ))}
+      </ol>
     </nav>
   );
 };
