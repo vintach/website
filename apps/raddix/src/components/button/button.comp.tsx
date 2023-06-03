b646595fe250e013167acca28ea6b67e6d66421b
realdiegor <diegoreyes150103@gmail.com>
2023-06-02 23:52:07 -0400

    refactor: make new design of homepage
    Change button component used within Homepage. Deleted old sass styles files and added tailwind styles.

--- apps/raddix/src/components/button/button.comp.tsx
+++ apps/raddix/src/components/button/button.comp.tsx
@@ -1,14 +1,23 @@
 import Link from 'next/link';
-import styles from './button.module.scss';
 
 interface ButtonProps {
   text: string;
   to?: string;
+  type: 'primary' | 'secondary';
 }
 
-export const Button = ({ text, to = '/' }: ButtonProps) => {
+export const Button = ({ text, type, to = '/' }: ButtonProps) => {
+  const colorStyles = {
+    primary: 'bg-purple-60 border-0 hover:bg-purple-70',
+    secondary:
+      'bg-transparent border border-gray-100 border-solid hover:bg-gray-120'
+  };
+
   return (
-    <Link href={to} className={styles.button}>
+    <Link
+      href={to}
+      className={`cursor-pointer rounded-3xl px-7 py-2.5 text-sm duration-100 ${colorStyles[type]}`}
+    >
       {text}
     </Link>
   );
--- apps/raddix/src/components/hero/hero.comp.tsx
+++ apps/raddix/src/components/hero/hero.comp.tsx
@@ -1,17 +1,41 @@
 import type { HeroProps } from '@/types/home-data';
+import Image from 'next/image';
 import { Button } from '../button';
-import styles from './hero.module.scss';
 
 export const Hero = ({ title, description, button }: HeroProps) => {
   return (
-    <div className={styles.hero}>
-      <h1>
-        {title} <span>design system.</span>
-      </h1>
-      <p>{description}</p>
-      <Button text={button} to='/docs/get-started/create-component' />
-      {/* <div className={styles.bgGrad} /> */}
-      {/* <div className={styles.bgGrad2} /> */}
-    </div>
+    <>
+      <div className='relative mx-auto mb-28 mt-[min(15vw,_100px)] flex w-full flex-col items-center overflow-visible px-sm text-center md:flex-row-reverse'>
+        <div className='relative mb-10 h-40 w-40 md:grid md:w-1/2 md:place-content-center'>
+          <div className='absolute h-full w-full rounded-full bg-gradient-to-br from-blue-50 to-purple-50 blur-[100px] md:inset-0 md:m-auto md:h-96 md:w-96 md:opacity-75'></div>
+          <div className='relative h-full w-full bg-gradient-to-t md:inset-1 md:h-72 md:w-72'>
+            <Image
+              src='/raddix.svg'
+              alt='Raddix Logo'
+              fill
+              sizes='(max-width: 900px) 160px, 300px'
+            />
+          </div>
+        </div>
+        <div className='md:w-1/2 md:text-start'>
+          <h1 className='mx-auto mb-16 max-w-5xl text-[clamp(3rem,_5vw,_5rem)] capitalize'>
+            {title}
+          </h1>
+          <p className='mb-16 text-md opacity-80'>{description}</p>
+          <div className='flex flex-wrap justify-center gap-5 md:justify-start'>
+            <Button
+              type='primary'
+              text={button}
+              to='/docs/get-started/create-component'
+            />
+            <Button
+              type='secondary'
+              text='GitHub'
+              to='https://github.com/vintach/raddix'
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
