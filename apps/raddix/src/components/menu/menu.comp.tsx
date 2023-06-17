a1298ff131ce5e6c8f1c466c78f5d46ad2a6da22
realdiegor <diegoreyes150103@gmail.com>
2023-06-16 23:17:50 -0400

    refactor: menu component styles

--- apps/raddix/src/components/menu/menu.comp.tsx
+++ apps/raddix/src/components/menu/menu.comp.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
+import { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import React, { useCallback, useEffect, useState } from 'react';
-import styles from './menu.module.scss';
-import nextConfig from '../../../next.config';
+import Link from 'next/link';
+import nextConfig from 'next.config';
 
 interface MenuItems {
   name: string;
@@ -16,7 +15,7 @@ export const Menu = () => {
   const getMenuItems = useCallback(async () => {
     const realLocale = locale ?? nextConfig.i18n.defaultLocale;
     const response: { navList: MenuItems[] } = await import(
-      `../../../data/menu/${realLocale}.json`
+      `data/menu/${realLocale}.json`
     );
     setMenuItems(response.navList);
   }, [locale]);
@@ -26,11 +25,18 @@ export const Menu = () => {
   }, [locale, getMenuItems]);
 
   return (
-    <nav className={styles.nav}>
-      <ul>
+    <nav className='w-full justify-end border-solid border-white/5 sm:mr-sm sm:w-auto sm:border-r sm:pr-lg'>
+      <ul className='flex w-full flex-col items-center justify-center sm:flex-row sm:gap-lg'>
         {menuItems.map((item, id) => (
-          <li key={id}>
-            <Link href={item.path} locale={locale}>
+          <li
+            key={id}
+            className='opacity-1 w-full list-none pb-xs text-sm transition-opacity duration-100 first:m-0 hover:opacity-80 sm:w-auto sm:pb-0'
+          >
+            <Link
+              href={item.path}
+              locale={locale}
+              className='block py-xs sm:inline sm:py-sm'
+            >
               {item.name}
             </Link>
           </li>
