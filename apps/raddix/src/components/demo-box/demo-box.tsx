2dd5d47be23b986010210975aee66c6fcd3b2b8c
realdiegor <diegoreyes150103@gmail.com>
2023-06-20 21:39:57 -0400

    chore: remove unused demo-box component

--- apps/raddix/src/components/demo-box/demo-box.tsx
+++
@@ -1,6 +0,0 @@
-import type { Children } from '@/types/global';
-import styles from './demo-box.module.scss';
-
-export const DemoBox = ({ children }: Children) => {
-  return <div className={styles.container}>{children}</div>;
-};
