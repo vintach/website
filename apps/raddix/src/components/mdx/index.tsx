7db99e24dd5818499a5de13dd79c3a76caaf45a6
realdiegor <diegoreyes150103@gmail.com>
2023-06-21 23:03:58 -0400

    refactor: mdx component styles

--- apps/raddix/src/components/mdx/index.tsx
+++ apps/raddix/src/components/mdx/index.tsx
@@ -1,10 +1,8 @@
-import type { Language } from 'prism-react-renderer';
 import type { ReactNode } from 'react';
-import Highlight, { defaultProps } from 'prism-react-renderer';
+import type { Language } from 'prism-react-renderer';
 import { useState } from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/dracula';
-import classNames from 'classnames';
-import styles from './mdx.module.scss';
 import { componentsDemo } from '@/demo';
 import { Card, CardGroup } from '../card';
 import { ApiTable } from '../api-table/api-table';
@@ -43,10 +41,9 @@ export const TextPre = (props: Children) => {
   const classNameRo = children.props.className || '';
   const code = children.props.children.trim();
   const language = classNameRo.replace(/language-/, '');
-  const btnClass = classNames(styles.textPreBtn, {
-    [styles.textPreBtnSuccess]: isCopied
-  });
-
+  const copyButtonStyles = isCopied
+    ? 'bg-blue-60 text-white'
+    : 'bg-white/60 text-gray-80';
   const handleCopy = (str: string) => {
     if (isCopied) {
       return;
@@ -61,8 +58,11 @@ export const TextPre = (props: Children) => {
   };
 
   return (
-    <div className={styles.textPre}>
-      <button className={btnClass} onClick={() => handleCopy(code)}>
+    <div className='group relative my-6 overflow-auto rounded-lg border border-solid border-white/10 bg-white/5 px-8 py-5 text-[1.05rem] scrollbar-thumb-gray-40 scrollbar-track-rounded-xl'>
+      <button
+        className={`absolute right-8 top-4 cursor-pointer rounded-lg border-0 px-2 py-1 text-sm opacity-0 transition-all duration-200 ease-in group-hover:opacity-100 active:scale-95 ${copyButtonStyles}`}
+        onClick={() => handleCopy(code)}
+      >
         {isCopied ? '🎉 Copied!' : 'Copy'}
       </button>
       <Highlight
@@ -100,28 +100,49 @@ export const TextPre = (props: Children) => {
 
 export const MDXComponents = {
   h1: ({ children, ...props }: Children) => (
-    <Text as='h1' className={styles.title} {...props}>
+    <Text
+      as='h1'
+      className='mb-md text-[clamp(38px,_45vw,_42px)] font-bold'
+      {...props}
+    >
       {children}
     </Text>
   ),
   h2: ({ children, ...props }: Children) => (
-    <Text as='h2' className={styles.subTitle} isTitle {...props}>
+    <Text
+      as='h2'
+      className='mb-sm mt-3xl text-[clamp(28px,_32vw,_32px)] font-bold'
+      isTitle
+      {...props}
+    >
       {children}
     </Text>
   ),
   h3: ({ children, ...props }: Children) => (
-    <Text as='h3' className={styles.h3} isTitle {...props}>
+    <Text
+      as='h3'
+      className='mb-sm mt-xl text-[clamp(19px,_32vw,_23px)] font-semibold'
+      isTitle
+      {...props}
+    >
       {children}
     </Text>
   ),
   p: ({ children }: Children) => (
-    <Text as='p' className={styles.paragraph}>
+    <Text
+      as='p'
+      className='text-[clamp(16px,_18vw,_18px)] leading-8 opacity-75'
+    >
       {children}
     </Text>
   ),
   pre: TextPre,
-  ul: ({ children }: Children) => <ul className={styles.ul}>{children}</ul>,
-  li: ({ children }: Children) => <li className={styles.li}>{children}</li>,
+  ul: ({ children }: Children) => <ul className='my-md pl-lg'>{children}</ul>,
+  li: ({ children }: Children) => (
+    <li className='mb-xs list-disc text-[clamp(16px,_18vw,_18px)] opacity-75'>
+      {children}
+    </li>
+  ),
   Card,
   CardGroup,
   ApiTable,
