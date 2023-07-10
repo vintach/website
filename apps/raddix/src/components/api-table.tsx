7462a3e0eb46b33d57f96a2c5c8caa7f6d22f2d0
realdiegor <diegoreyes150103@gmail.com>
2023-07-10 18:08:10 -0400

    feature: add more detail fields in api-table component
    change tailwind style "ax1xa" to "auto minmax(0, 1fr) auto" value because table would extend and so the article tag. Now it respects available space

--- apps/raddix/src/components/api-table.tsx
+++ apps/raddix/src/components/api-table.tsx
@@ -1,6 +1,11 @@
+import { useRouter } from 'next/router';
+
 interface Option {
   name: string;
   description: string;
+  required?: string;
+  defaultValue?: string;
+  type: string;
 }
 
 export interface ApiTableProps {
@@ -10,28 +15,56 @@ export interface ApiTableProps {
 
 export const ApiTable = ({ head, data }: ApiTableProps) => {
   return (
-    <table className='mt-sm w-full border-collapse text-left'>
+    <table className='mt-sm block w-full table-fixed border-collapse overflow-x-auto text-left scrollbar-thin scrollbar-track-[transparent] scrollbar-thumb-gray-40/50 scrollbar-thumb-rounded-lg'>
       <thead>
         <tr>
-          <th className='w-1/4 bg-white/5 p-xs text-sm font-medium'>
+          <th className='whitespace-nowrap bg-white/5 p-xs text-sm font-medium'>
             {head?.name ?? 'Name'}
           </th>
-          <th className='w-3/4 bg-white/5 p-xs text-sm font-medium'>
+          <th className='whitespace-nowrap bg-white/5 p-xs text-sm font-medium'>
             {head?.description ?? 'Description'}
+          </th>
+          {(head?.required || data[0].required) && (
+            <th className='whitespace-nowrap bg-white/5 p-xs text-sm font-medium'>
+              {head?.required ?? 'Required'}
+            </th>
+          )}
+          {(head?.defaultValue || data[0].defaultValue) && (
+            <th className='whitespace-nowrap bg-white/5 p-xs text-sm font-medium'>
+              {head?.defaultValue ?? 'Default Value'}
+            </th>
+          )}
+          <th className='whitespace-nowrap bg-white/5 p-xs text-sm font-medium'>
+            {head?.type ?? 'Type'}
           </th>
         </tr>
       </thead>
       <tbody>
-        {data.map(({ name, description }, index) => (
-          <tr key={`${name}-${index}`}>
-            <td className='w-1/4 border-b border-solid border-white/5 px-xs py-sm text-xs '>
-              {name}
-            </td>
-            <td className='w-3/4 border-b border-solid border-white/5 px-xs py-sm text-xs '>
-              {description}
-            </td>
-          </tr>
-        ))}
+        {data.map(
+          ({ name, description, required, defaultValue, type }, index) => (
+            <tr key={`${name}-${index}`} className=''>
+              <td className='border-b border-solid border-white/5 px-xs py-sm text-xs'>
+                {name}
+              </td>
+              <td className='w-5/6 min-w-[25ch] border-b border-solid border-white/5 px-xs py-sm text-xs '>
+                {description}
+              </td>
+              {(head?.required || data[0].required) && (
+                <td className='border-b border-solid border-white/5 px-xs py-sm text-xs '>
+                  {required}
+                </td>
+              )}
+              {(head?.defaultValue || data[0].defaultValue) && (
+                <td className='border-b border-solid border-white/5 px-xs py-sm text-xs '>
+                  {defaultValue}
+                </td>
+              )}
+              <td className='border-b border-solid border-white/5 px-xs py-sm text-xs'>
+                {type}
+              </td>
+            </tr>
+          )
+        )}
       </tbody>
     </table>
   );
