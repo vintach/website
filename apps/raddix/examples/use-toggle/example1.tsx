160b46ede12ad888490130c138eaf5e426774231
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-07-18 21:11:03 -0500

    fix: add missing content and typography correction

--- apps/raddix/examples/use-toggle/example1.tsx
+++ apps/raddix/examples/use-toggle/example1.tsx
@@ -12,14 +12,14 @@ export const Example = () => {
         saepe dolores?
       </p>
 
-      {show ? (
+      {show && (
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum earum
           dolore, nostrum esse sint nobis facere consequuntur placeat harum
           molestiae unde ad laudantium reiciendis, a tempore eos, sunt quasi
           quo.
         </p>
-      ) : null}
+      )}
 
       <span onClick={toggle}>{!show ? 'Read more...' : 'Read less'}</span>
     </div>
