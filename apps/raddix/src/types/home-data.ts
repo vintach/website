39b426645b960ded5491eef9f450b7b2bf4e4851
Moises Machuca Valverde <rmoimv@gmail.com>
2023-09-25 16:50:47 -0500

    feat: add features data

--- apps/raddix/src/types/home-data.ts
+++ apps/raddix/src/types/home-data.ts
@@ -1,31 +1,15 @@
-interface Content {
-  antisubtitle: string;
-  subtitle: string;
-  description: string;
-}
-
 export interface HeroProps {
   title: string;
   description: string;
   button: string;
 }
 
-interface DataCard {
+export interface Feature {
   title: string;
-  icon: string;
   description: string;
 }
 
-export interface AccessibleProps extends Content {
-  main: {
-    dataCard: DataCard[];
-  };
-}
-
-export interface StylingProps extends Content {}
-
 export interface HomeData {
   hero: HeroProps;
-  accessible: AccessibleProps;
-  styling: StylingProps;
+  features: Feature[];
 }
