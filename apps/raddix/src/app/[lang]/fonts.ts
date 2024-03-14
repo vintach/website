cc5a17eb59563e28f907499f3e49b494603bc0fa
Moises Machuca Valverde <rmoimv@gmail.com>
2024-03-14 15:02:31 -0500

    feat: Optimizing sources and adding them to tailwindcss

---
+++ apps/raddix/src/app/[lang]/fonts.ts
@@ -0,0 +1,21 @@
+import { Inter, JetBrains_Mono, Days_One } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap'
+});
+
+export const jetbrainsMono = JetBrains_Mono({
+  weight: ['400', '500', '600'],
+  display: 'swap',
+  variable: '--font-jetbrains-mono'
+});
+
+export const daysOne = Days_One({
+  weight: ['400'],
+  display: 'swap',
+  variable: '--font-days-one'
+});
+
+export const fonts = `${inter.variable} ${jetbrainsMono.variable} ${daysOne.variable}`;
