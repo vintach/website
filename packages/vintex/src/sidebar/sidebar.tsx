dc0a1648d99f2ffa1ad8bfa817e910ccded5abe0
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-06 22:57:23 -0500

    remove sidebar component of raddix

--- packages/vintex/src/sidebar/sidebar.tsx
+++ packages/vintex/src/sidebar/sidebar.tsx
@@ -13,7 +13,7 @@ export const Sidebar = ({ items, lang, defaultLang }: SidebarProps) => {
   const currentPath = usePathname();
 
   return (
-    <aside className='sticky top-[125px] hidden h-[calc(100vh-125px)] w-64 overflow-y-auto scrollbar-thin scrollbar-track-gray-20/10 scrollbar-thumb-gray-40/50 scrollbar-track-rounded scrollbar-thumb-rounded-lg md:block'>
+    <aside className='ui-sticky ui-top-[125px] ui-hidden ui-h-[calc(100vh-125px)] ui-w-64 ui-overflow-y-auto ui-scrollbar-thin ui-scrollbar-track-gray-20/10 ui-scrollbar-thumb-gray-40/50 ui-scrollbar-track-rounded ui-scrollbar-thumb-rounded-lg md:ui-block'>
       <Tree
         items={items}
         activeItem={currentPath}
