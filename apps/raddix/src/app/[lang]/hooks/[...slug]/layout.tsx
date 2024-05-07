35698ef35b9946c9bd74bd1b5b05683f120c77a7
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-06 22:32:15 -0500

    update sidebar in layouts

--- apps/raddix/src/app/[lang]/hooks/[...slug]/layout.tsx
+++ apps/raddix/src/app/[lang]/hooks/[...slug]/layout.tsx
@@ -1,5 +1,6 @@
 import { getConfigFileRepo } from '@/lib/content';
-import { Sidebar, SidebarMenu, Pagination, PageNav } from 'vintex';
+import { Sidebar } from 'vintex';
+import { defaultLocale } from '@/i18n';
 
 export default async function HooksLayout({
   children,
@@ -15,15 +16,9 @@ export default async function HooksLayout({
   });
 
   return (
-    <main className='mx-auto max-w-std gap-2xl px-sm py-xl md:grid md:grid-cols-ax1 lg:grid-cols-ax1xa'>
-      <Sidebar>
-        <SidebarMenu items={config.sidebar} />
-      </Sidebar>
-      <div>
-        {children}
-        <Pagination menu={config.sidebar} />
-      </div>
-      <PageNav locale={lang} />
-    </main>
+    <div className='mx-auto max-w-std gap-2xl px-sm py-xl md:grid md:grid-cols-ax1'>
+      <Sidebar items={config.sidebar} lang={lang} defaultLang={defaultLocale} />
+      {children}
+    </div>
   );
 }
