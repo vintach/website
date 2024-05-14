afa91e54feaf75ef13d15d81fd6c441e9530f7a5
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-13 22:18:07 -0500

    add meta in options

---
+++ apps/raddix/content/site/_config.ts
@@ -0,0 +1,13 @@
+import { type Config } from '@/types/content';
+
+export const configSite: Config = {
+  sidebar: [],
+  meta: {
+    title: 'Raddix',
+    url: 'https://raddix.dev',
+    author: {
+      name: 'Moises Machuca Valverde',
+      url: 'https://moisesmachuca.com'
+    }
+  }
+};
