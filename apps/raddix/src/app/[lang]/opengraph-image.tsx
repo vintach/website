e0570e929915d38c32412e711b783a9e34def4b4
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-19 12:25:22 -0500

    add opengraph-image and twitter-image

---
+++ apps/raddix/src/app/[lang]/opengraph-image.tsx
@@ -0,0 +1,35 @@
+import { ImageResponse } from 'next/og';
+import { getConfigFile } from '@/lib/content';
+import { OG } from '@/components/og';
+import { configSite } from 'content/site/_config';
+import { getFileSync } from '@/utils/get-file';
+
+interface Props {
+  params: { lang: string };
+}
+
+export const alt = 'Raddix Home Page';
+export const size = {
+  width: 1200,
+  height: 630
+};
+export const contentType = 'image/png';
+
+export default function Image({ params: { lang } }: Props) {
+  const interBold = getFileSync('assets', 'Inter-Bold.ttf');
+  const { meta } = getConfigFile({ lang, dirPath: 'content/site' });
+
+  return new ImageResponse(
+    <OG preTitle={configSite.meta?.title} title={meta?.description} />,
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'InterBold',
+          data: interBold,
+          style: 'normal'
+        }
+      ]
+    }
+  );
+}
---
+++ apps/raddix/src/components/og.tsx
@@ -0,0 +1,84 @@
+import { configSite } from 'content/site/_config';
+
+interface OGProps {
+  preTitle?: string;
+  title?: string;
+  description?: string;
+}
+
+export const OG = ({ title, preTitle, description }: OGProps) => {
+  const site = configSite.meta;
+
+  return (
+    <div
+      style={{
+        background: '#0a0a10',
+        backgroundImage: `url("${site?.url}/og_base.png")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        width: 1200,
+        height: 630,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          paddingLeft: '80px',
+          width: '55%',
+          boxSizing: 'border-box'
+        }}
+      >
+        {preTitle && (
+          <span
+            style={{
+              display: 'block',
+              fontFamily: 'InterBold',
+              fontWeight: 700,
+              fontSize: 78,
+              letterSpacing: '-.04em',
+              lineHeight: '78px',
+              color: '#00000000',
+              backgroundClip: 'text',
+              backgroundImage:
+                'linear-gradient(to bottom right, #4341f5, #9153fe)',
+              marginBottom: '10px'
+            }}
+          >
+            {preTitle}
+          </span>
+        )}
+        <h1
+          style={{
+            fontFamily: 'InterBold',
+            fontSize: 78,
+            fontWeight: 700,
+            letterSpacing: '-.04em',
+            lineHeight: '80px',
+            color: '#f0f0f0',
+            marginBottom: '17px'
+          }}
+        >
+          {title ?? site?.title}
+        </h1>
+        {description && (
+          <p
+            style={{
+              fontFamily: 'InterRegular',
+              fontSize: 32,
+              color: '#cececf',
+              letterSpacing: '-.01em',
+              lineHeight: '40px'
+            }}
+          >
+            {description}
+          </p>
+        )}
+      </div>
+      <div style={{ width: '45%' }}></div>
+    </div>
+  );
+};
