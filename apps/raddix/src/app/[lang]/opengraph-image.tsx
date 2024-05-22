0e74b8471984962baadda291b4b64403e7e50121
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-21 23:32:22 -0500

    change openGraph synchronously

--- apps/raddix/src/app/[lang]/opengraph-image.tsx
+++ apps/raddix/src/app/[lang]/opengraph-image.tsx
@@ -2,7 +2,8 @@ import { ImageResponse } from 'next/og';
 import { getConfigFile } from '@/lib/content';
 import { OG } from '@/components/og';
 import { configSite } from 'content/site/_config';
-import { getFile } from '@/utils/get-file';
+import fs from 'fs';
+import path from 'path';
 
 interface Props {
   params: { lang: string };
@@ -15,8 +16,11 @@ export const size = {
 };
 export const contentType = 'image/png';
 
-export default async function Image({ params: { lang } }: Props) {
-  const interBold = await getFile('assets/Inter-Bold.ttf');
+const interBoldPath = path.join(process.cwd(), 'assets', 'inter-bolder.ttf');
+
+export default function Image({ params: { lang } }: Props) {
+  const interBold = fs.readFileSync(interBoldPath);
+
   const { meta } = getConfigFile({ lang, dirPath: 'content/site' });
 
   return new ImageResponse(
