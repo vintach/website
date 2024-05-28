097208d98a329940d276ea633aa7552200ee8584
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-28 16:44:07 -0500

    update og-image in hook docs

--- apps/raddix/src/app/[lang]/hooks/[slug]/opengraph-image.tsx
+++ apps/raddix/src/app/[lang]/hooks/[slug]/opengraph-image.tsx
@@ -2,12 +2,19 @@ import { ImageResponse } from 'next/og';
 import { OG } from '@/components/og';
 import { matter } from '@/lib/matter';
 import { type MetaOptions } from '@/types/content';
+import { getRemoteFile } from '@/utils/get-remote-file';
 interface Props {
   params: { slug: string; lang: string };
 }
 
+const configRepo = {
+  repo: 'raddix',
+  owner: 'vintach',
+  branch: 'main'
+};
+
 export const runtime = 'edge';
-export const alt = 'Hook Documentation';
+export const alt = 'Hook Docs';
 export const size = {
   width: 1200,
   height: 630
@@ -21,17 +28,11 @@ export default async function Image({ params }: Props) {
   const interRegular = fetch(
     new URL('../../../../../assets/Inter-Regular.ttf', import.meta.url)
   ).then(res => res.arrayBuffer());
-  const url = new URL(
-    `vintach/raddix/main/docs/${params.lang}/${params.slug}.mdx`,
-    'https://raw.githubusercontent.com/'
-  );
-
-  const response = await fetch(url.toString(), {
-    headers: {
-      Accept: 'application/vnd.github.v3.raw'
-    }
+  const response = await getRemoteFile({
+    ...configRepo,
+    filePath: `docs/${params.lang}/${params.slug}.mdx`
   });
-  const { meta } = matter<MetaOptions>(await response.text());
+  const { meta } = matter<MetaOptions>(response);
   const { title, description } = meta;
 
   return new ImageResponse(<OG title={title} description={description} />, {
