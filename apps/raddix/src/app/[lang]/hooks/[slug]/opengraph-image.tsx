da9e4521958147f6ed9bf8e1afae5eb65ab0bc0e
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-28 19:38:53 -0500

    update og-image hooks

--- apps/raddix/src/app/[lang]/hooks/[slug]/opengraph-image.tsx
+++ apps/raddix/src/app/[lang]/hooks/[slug]/opengraph-image.tsx
@@ -25,21 +25,27 @@ export default async function Image({ params }: Props) {
   const inter = fetch(
     new URL('../../../../../assets/Inter-Regular.ttf', import.meta.url)
   ).then(res => res.arrayBuffer());
+  const interBold = fetch(
+    new URL('../../../../../assets/Inter-Bold.ttf', import.meta.url)
+  ).then(res => res.arrayBuffer());
   const response = await getRemoteFile({
     ...configRepo,
     filePath: `docs/${params.lang}/${params.slug}.mdx`
   });
-  const { meta } = matter<MetaOptions>(response);
-  const { title, description } = meta;
+  const {
+    meta: { title, description }
+  } = matter<MetaOptions>(response);
 
   return new ImageResponse(<OG title={title} description={description} />, {
     ...size,
     fonts: [
       {
         name: 'Inter',
-        data: await inter,
-        style: 'normal'
-      }
+        data: await interBold,
+        style: 'normal',
+        weight: 700
+      },
+      { name: 'Inter', data: await inter, style: 'normal', weight: 400 }
     ]
   });
 }
