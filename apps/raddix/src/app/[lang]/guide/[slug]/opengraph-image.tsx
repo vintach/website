bffe94c9d83cbbc75cd5942df25c9a0846958645
Moises Machuca Valverde <rmoimv@gmail.com>
2024-05-28 20:00:59 -0500

    change font in og-image of guide

--- apps/raddix/src/app/[lang]/guide/[slug]/opengraph-image.tsx
+++ apps/raddix/src/app/[lang]/guide/[slug]/opengraph-image.tsx
@@ -16,19 +16,13 @@ export const size = {
 
 export default async function Image({ params: { lang, slug } }: Props) {
   const inter = fetch(
-    new URL('../../../../../assets/Inter-Regular.ttf', import.meta.url)
+    new URL('../../../../../assets/Inter-Medium.ttf', import.meta.url)
   ).then(res => res.arrayBuffer());
 
   const { title, description } = await getGuide(lang, slug);
 
   return new ImageResponse(<OG title={title} description={description} />, {
     ...size,
-    fonts: [
-      {
-        name: 'Inter',
-        data: await inter,
-        style: 'normal'
-      }
-    ]
+    fonts: [{ name: 'Inter', data: await inter, style: 'normal', weight: 500 }]
   });
 }
