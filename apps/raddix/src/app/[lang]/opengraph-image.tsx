6892693292db16dd59ad37419065f59d043da956
Moises Machuca Valverde <rmoimv@gmail.com>
2024-06-04 18:37:00 -0500

    Testing without calling files using readfile

--- apps/raddix/src/app/[lang]/opengraph-image.tsx
+++ apps/raddix/src/app/[lang]/opengraph-image.tsx
@@ -1,8 +1,5 @@
 import { ImageResponse } from 'next/og';
-import { getConfigFile } from '@/lib/content';
 import { OG } from '@/components/og';
-import { configSite } from 'content/site/_config';
-import { readFile } from 'node:fs/promises';
 import fetch from 'node-fetch';
 
 interface Props {
@@ -20,10 +17,9 @@ export default async function Image({ params: { lang } }: Props) {
   const interBold = await fetch(
     'https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/latin-700-normal.ttf'
   );
-  const { meta } = getConfigFile({ lang, dirPath: 'content/site' });
 
   return new ImageResponse(
-    <OG preTitle={configSite.meta?.title} title={meta?.description} />,
+    <OG preTitle={'Raddix'} title={'Collection of essential React Hooks'} />,
     {
       ...size,
       fonts: [
