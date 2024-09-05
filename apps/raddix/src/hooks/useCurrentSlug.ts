503e6533d825a798b202a12319998730a3470938
Moises Machuca Valverde <rmoimv@gmail.com>
2024-09-04 23:40:45 -0500

    fix: 'query' is possibly 'null'.

--- apps/raddix/src/hooks/useCurrentSlug.ts
+++ apps/raddix/src/hooks/useCurrentSlug.ts
@@ -2,7 +2,7 @@ import { useSearchParams } from 'next/navigation';
 
 export const useCurrentSlug = () => {
   const query = useSearchParams();
-  let slug = query.get('slug');
+  let slug = query?.get('slug');
   if (Array.isArray(slug)) {
     return (slug = `/docs/${slug.join('/')}`);
   }
